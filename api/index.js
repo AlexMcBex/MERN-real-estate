8c4e6235ae9d792df73ca22c02daee41dcea1884
AlexMcBex <alessiobelli94@gmail.com>
2023-12-04 11:50:48 -0500

    added error handling in mongoose.connect

--- api/index.js
+++ api/index.js
@@ -3,10 +3,17 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config( )
 
-mongoose.connect(process.env.MONGODB)
+mongoose
+.connect(process.env.MONGODB)
+.then(()=>{
+    console.log('=== Connected to MongoDB ===')
+})
+.catch((err)=>{
+    console.log(`!!! ${err} !!!`)
+})
 
 const app = express()
 
 app.listen(3000, () =>{
-    console.log('(((Server running on port 3000)))')
+    console.log('=== Server running on port 3000 ===')
 })
