155d5e9d8df3d5aebe9ef9c59bd16eb39990eb51
AlexMcBex <alessiobelli94@gmail.com>
2023-12-05 16:03:54 -0500

    data from req.body is sent to the database

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 
 mongoose
@@ -14,6 +15,8 @@ mongoose
   });
 
 const app = express();
+// express.json turns undefined info to json format info
+app.use(express.json())
 
 app.listen(3000, () => {
   console.log("=== Server running on port 3000 ===");
@@ -21,3 +24,4 @@ app.listen(3000, () => {
 
 // API User Route
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
