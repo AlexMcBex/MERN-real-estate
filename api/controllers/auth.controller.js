daef8001982ce4e336e6ad6231cfa275e601349e
AlexMcBex <alessiobelli94@gmail.com>
2023-12-05 16:10:53 -0500

    added bcryptjs to encrypt passwords

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,11 @@
 import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs'
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
-
-  const newUser = new User({ username, email, password });
+    // hash password 10 times to make it encrypted
+    const hashedPassword = await bcryptjs.hashSync(password, 10)
+  const newUser = new User({ username, email, password: hashedPassword });
 
   await newUser.save();
   res.status(201).json('User Created successfully')
