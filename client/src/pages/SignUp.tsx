9c8ab7e63c80b11fadac04ee412fee7316ead540
AlexMcBex <alessiobelli94@gmail.com>
2023-12-06 20:21:45 -0500

    signup page can create user in DB

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e: { target: { id: any; value: any; }; }) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e: { preventDefault: () => void; })=>{
+    // prevents page refresh on React
+    e.preventDefault()
+    // fetch info from api
+    const res = await fetch("/api/auth/signup", {
+      // change method to POST
+      method: 'POST',
+      // add headers
+      headers: {
+        'content-type': 'application/json'
+      },
+      // stringify result to json
+      body: JSON.stringify(formData)
+    })
+    const data= await res.json()
+    console.log(data)
+  }
+  console.log(formData)
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
       <form
-        action="
-      "
+        onSubmit={handleSubmit}
         className="flex flex-col gap-4 "
       >
         <input
@@ -15,18 +40,21 @@ export default function SignUp() {
           placeholder="username"
           className="border p-3 rounded-lg "
           id="username"
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="email"
           className="border p-3 rounded-lg "
           id="email"
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="password"
           className="border p-3 rounded-lg "
           id="password"
+          onChange={handleChange}
         />
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-80">
           Sign Up
