85e6c949e24548ab51da755de4eb4edbe6872ff3
AlexMcBex <alessiobelli94@gmail.com>
2023-12-07 15:01:07 -0500

    prepared controller for signin (email pass checker)

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -14,3 +14,17 @@ export const signup = async (req, res, next) => {
     next(err);
   }
 };
+
+export const signin = async(req, res, next)=>{
+  const {email, password}= req.body
+  try{
+    // check if email exists or not hten same for the password
+    const validUser = await User.findOne({email})
+    if (!validUser) return next(errorHandler(404, 'User not found!'))
+    // bcrypt method to compare hashed password
+    const validPassword = bcryptjs.compareSync(password, validUser.password)
+    if(!validPassword) return next(errorHandler(401, 'Wrong credentials!'))
+  }catch(error){
+    next(error)
+  }
+}
