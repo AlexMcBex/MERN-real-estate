6143be210c186d1b0201a3b2e2a53cd279bec643
AlexMcBex <alessiobelli94@gmail.com>
2023-12-07 11:05:18 -0500

    error is displayed on signup page

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -3,38 +3,51 @@ import { Link } from "react-router-dom";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
-  const handleChange = (e: { target: { id: any; value: any; }; }) => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e: { target: { id: any; value: any } }) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
-  const handleSubmit = async (e: { preventDefault: () => void; })=>{
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
     // prevents page refresh on React
-    e.preventDefault()
-    // fetch info from api
-    const res = await fetch("/api/auth/signup", {
-      // change method to POST
-      method: 'POST',
-      // add headers
-      headers: {
-        'content-type': 'application/json'
-      },
-      // stringify result to json
-      body: JSON.stringify(formData)
-    })
-    const data= await res.json()
-    console.log(data)
-  }
-  console.log(formData)
+    e.preventDefault();
+    try {
+      setLoading(true);
+      // fetch info from api
+      const res = await fetch("/api/auth/signup", {
+        // change method to POST
+        method: "POST",
+        // add headers
+        headers: {
+          "content-type": "application/json",
+        },
+        // stringify result to json
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      // check if data has an error
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+    } catch (error: any) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+  // console.log(formData);
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col gap-4 "
-      >
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
         <input
           type="text"
           placeholder="username"
@@ -56,16 +69,20 @@ export default function SignUp() {
           id="password"
           onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-80">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
-      <div>
+      <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to={"/sign-in"}>
           <span className="text-blue-700">Sign In</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
