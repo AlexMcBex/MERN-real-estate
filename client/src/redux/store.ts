df347f4b521935809206cd11b376d8f0ac0b182a
AlexMcBex <alessiobelli94@gmail.com>
2023-12-13 16:24:06 -0500

    added userReducer to store in redux

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import userReducer from '../redux/user/userSlice.ts'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {user: userReducer},
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false
   }),
