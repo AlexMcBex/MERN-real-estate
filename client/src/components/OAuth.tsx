708f478b5423b9f96f142f9e799d3f2ce7177f07
AlexMcBex <alessiobelli94@gmail.com>
2024-01-08 11:39:34 -0500

    started firebase config

--- client/src/components/OAuth.tsx
+++ client/src/components/OAuth.tsx
@@ -1,7 +1,14 @@
 // import React from 'react'
 
 export default function OAuth() {
+    const handleGoogleClick = async () =>{
+        try{
+            
+        }catch(err){
+            console.log('could not login with Google' , err)
+        }
+    }
   return (
-    <div>OAuth</div>
+    <button onClick={handleGoogleClick} type='button' className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95">Continue with Google</button>
   )
 }
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: "mern-real-estate-6ce06.firebaseapp.com",
+  projectId: "mern-real-estate-6ce06",
+  storageBucket: "mern-real-estate-6ce06.appspot.com",
+  messagingSenderId: "755473657184",
+  appId: "1:755473657184:web:7edfd2cd7e66656330c205"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
