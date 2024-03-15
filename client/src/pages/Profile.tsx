8c6d54a76e05cbe153ffe237dca0a2f4ff29035e
AlexMcBex <alessiobelli94@gmail.com>
2024-03-15 11:37:37 -0400

    finished update user functionality

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -7,12 +7,17 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
-import { updateUserStart, updateUserSuccess, updateUserError } from "../redux/user/userSlice"; //redux
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserError,
+} from "../redux/user/userSlice"; //redux
 import { useDispatch } from "react-redux";
 
 export default function Profile() {
-  const { currentUser } = useSelector(
-    (state: { user: { currentUser: any } }) => state.user
+  const { currentUser, loading, error } = useSelector(
+    (state: { user: { currentUser: any; loading: boolean; error: any } }) =>
+      state.user
   );
   // in TS the pipe "|" character is an union, declares that a variable can be either one or the other type
   const [file, setFile] = useState<File | undefined>(undefined);
@@ -26,7 +31,9 @@ export default function Profile() {
   // we specify with this syntax that formData contains a avatar string optional property
   const [formData, setFormData] = useState<{ avatar?: string }>({});
 
-  const dispatch = useDispatch()
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+
+  const dispatch = useDispatch();
 
   // console.log(formData);
   // console.log(filePerc)
@@ -80,35 +87,31 @@ export default function Profile() {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
-
-  const handleSubmit = async (e:any) =>{
-    e.preventDefault()
-    try{
-      dispatch(updateUserStart())
-      const res = await fetch(`api/user/update/${currentUser._id}`,{
-        method: 'POST',
-        headers:{
-          'Content-Type': 'application/json'
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
-      const data = await res.json()
-      if (data.success === false){
-        dispatch(updateUserError(data.message))
-        return
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserError(data.message));
+        return;
       }
-      dispatch(updateUserSuccess(data))
-    }catch(error:any){
-      dispatch(updateUserError(error.message))
+      dispatch(updateUserSuccess(data));
+    } catch (error: any) {
+      dispatch(updateUserError(error.message));
     }
-  }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form 
-      className="flex flex-col gap-4"
-      onSubmit={handleSubmit}
-      >
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         {/* we reference to file Ref with ref to upload images */}
         <input
           onChange={(e) => {
@@ -165,14 +168,20 @@ export default function Profile() {
           className="border p-3 rounded-lg"
           id="password"
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Update
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Update"}
         </button>
-        <div className="flex justify-between mt-5">
-          <span className="text-red-700 cursor-pointer">Delete account</span>
-          <span className="text-red-700 cursor-pointer">Sign out</span>
-        </div>
       </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+      {/* the error here is defined in user.controller.js */}
+      <p className="text-red-500 mt-5">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5">{updateSuccess ? 'User updated successfully' : ''}</p>
     </div>
   );
 }
