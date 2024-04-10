cc12b2db3d47a5ecee95a1cd9a9ca8690c028839
AlexMcBex <alessiobelli94@gmail.com>
2024-04-10 11:12:57 -0400

    users can remove images during the image upload

--- client/src/pages/CreateListing.tsx
+++ client/src/pages/CreateListing.tsx
@@ -65,6 +65,14 @@ export default function CreateListing() {
     });
   };
 
+  const handleRemoveImage = (index: number) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
+  };
+  
+
   return (
     <main className="p-3 max-w-4xl  mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -197,7 +205,29 @@ export default function CreateListing() {
               Upload
             </button>
           </div>
-          <p className="text-red-700 text-sm">{imageUploadError && imageUploadError}</p>
+          <p className="text-red-700 text-sm">
+            {imageUploadError && imageUploadError}
+          </p>
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, index) => (
+              <div
+                key={url}
+                className="flex justify-between p-3 border items-center"
+              >
+                <img
+                  src={url}
+                  alt="listing image"
+                  className="w-20 h-20 object-contain rounded-lg"
+                />
+                <button
+                  onClick={() => handleRemoveImage(index)}
+                  type="button"
+                  className="p-3 text-red-700 rounded-lg uppercase hover:opacity-75"
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
           <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
             Create Listing
           </button>
