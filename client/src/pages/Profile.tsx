78e3031bf2563186f47a5d868de0dbd67cd30041
alexmcbex <alessiobelli94@gmail.com>
2024-11-09 12:04:39 -0500

    users can update their listings

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -324,7 +324,10 @@ export default function Profile() {
                 >
                   Delete
                 </button>
+                <Link to={`/update-listing/${listing._id}`}>
+
                 <button className="text-green-700 uppercase">Edit</button>
+                </Link>
               </div>
             </div>
           ))}
