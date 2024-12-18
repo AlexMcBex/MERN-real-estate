f52e510c3fe7f7f8678175f4e23372bdf7854b2e
alexmcbex <alessiobelli94@gmail.com>
2024-12-18 16:06:42 -0500

    made about page

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About <span className="text-slate-600">Truest</span>Estate</h1>
+      <p className="mb-4 text-slate-700">Truest Estate is your trusted platform for discovering, buying, selling, and renting properties with ease and confidence. <br /> Our mission is to simplify the real estate journey by connecting you with tailored listings, advanced search tools, and expert resources. <br />Whether you’re a first-time buyer, seasoned investor, or looking for your dream home, Truest Estate empowers you to make informed decisions every step of the way. Explore a seamless real estate experience built on transparency, trust, and innovation.</p>
+    </div>
   )
 }
